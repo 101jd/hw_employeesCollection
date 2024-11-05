35e2e010c06f5c9026b2425b0375981e7a33868f
JaneDoe <e.janedoe.87@gmail.com>
2024-11-05 18:06:03 +0300

    test

--- src/employees/Employee.java
+++ src/employees/Employee.java
@@ -1,2 +1,47 @@
-package employees;public class Employee {
+package employees;
+
+public class Employee {
+    String name;
+    int stage;
+    int table;
+    String phoneNumber;
+
+    public Employee(String name, int stage, int table, String phoneNumber){
+        this.name = name;
+        this.stage = stage;
+        this.table = table;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public int getTable() {
+        return table;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return new StringBuilder()
+                .append(name)
+                .append(System.lineSeparator())
+                .append(phoneNumber)
+                .append(System.lineSeparator())
+                .append("Stage: ")
+                .append(stage)
+                .append(System.lineSeparator())
+                .append("Table number: ")
+                .append(table)
+                .append(System.lineSeparator())
+                .toString();
+    }
 }
--- src/handbook/EmployeesHandBook.java
+++ src/handbook/EmployeesHandBook.java
@@ -1,2 +1,38 @@
-package handbook;public class EmployeesHandBook {
+package handbook;
+
+import employees.Employee;
+
+import java.util.*;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class EmployeesHandBook {
+    Set<Employee> employees;
+
+    public EmployeesHandBook(){
+        employees = new HashSet<>();
+    }
+
+    public EmployeesHandBook(Set<Employee> employees){
+        this.employees = employees;
+    }
+
+    public boolean addEmployee(Employee employee){
+        return employees.add(employee);
+    }
+
+    public Set<Employee> findByStage(int stage){
+        return employees.stream()
+                .filter(employee -> employee.getStage() == stage)
+                .collect(Collectors.toSet());
+    }
+
+    public Set<String> findPhoneByName(String name){
+        return employees.stream().filter(employee -> employee.getName().equals(name))
+                .map(employee -> employee.getPhoneNumber()).collect(Collectors.toSet());
+    }
+
+    public Employee findByTable(int table){
+        return employees.stream().filter(employee -> employee.getTable() == table).findAny().orElse(null);
+    }
 }
